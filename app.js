2d8183915845edf12748a738f458e14253e30768
冷冰 <lengbing@youku.com>
2014-01-10 19:40:57 +0800

    Update README.md

---
+++ app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var tv = require('./routes/tv');
+var client = require('./routes/client');
+
+
+
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/tv', tv.index);
+app.get('/client', client.index);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
