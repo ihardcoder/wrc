654f4012c38ca3eea47d8600a039751d83a1f079
Yuan Liang <yuanliangliang@youku.com>
2014-01-11 09:44:42 +0800

    add client.js, add client view

--- public/javascripts/tv.js
+++ public/javascripts/tv.js
@@ -1,9 +1,10 @@
 var myVideo = videojs("example_video_1");
 
 var socket = io.connect('http://localhost:3000');
-    socket.on('news', function (data) {
-    if(data.hello == 'world'){
-        myVideo.play();    
+
+socket.on('news', function (data) {
+    if(data.hello == 'world') {
+        myVideo.play();     
     }
     socket.emit('my other event', { my: 'data' });
 });
